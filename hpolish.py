8719ae3c7a0513845c45193fb7b3e1a09d3711d0
hybridpolaris <trinhquanghuy2701@gmail.com>
2026-02-02 10:56:35 +0700

    python hpolish, autodetect soft/hard c

--- hpolish.py
+++ hpolish.py
@@ -11,7 +11,7 @@ def handler(event: keyboard.KeyboardEvent):
 		lastKey = ""
 		return
 	
-	if event.name.lower() == lastKey.lower():
+	if event.name.lower() == lastKey.lower() and lastKey.lower() != "c":
 		keyboard.write("̄")
 		lastKey = ""
 		return
@@ -20,9 +20,14 @@ def handler(event: keyboard.KeyboardEvent):
 	match event.name.lower():
 		case "a": keyboard.write("A" if uppercase else "α")
 		case "b": keyboard.write("B" if uppercase else "β")
-		case "c": keyboard.write("C" if uppercase else "c")
+		case "c": keyboard.write("Ḱ" if uppercase else "κ́")
 		case "d": keyboard.write("Δ" if uppercase else "δ")
-		case "e": keyboard.write("E" if uppercase else "e")
+		case "e": 
+			if lastKey == "c":
+				keyboard.write("\b\bσ́")
+			elif lastKey == "C":
+				keyboard.write("\b\bΣ́" )
+			keyboard.write("E" if uppercase else "e")
 		case "f": keyboard.write("Φ" if uppercase else "φ")
 		case "g": 
 			if lastKey.lower() == "n": 
@@ -36,9 +41,9 @@ def handler(event: keyboard.KeyboardEvent):
 			elif lastKey == "T":
 				keyboard.write("\bΘ")
 			elif lastKey == "c":
-				keyboard.write("\bχ̌")
+				keyboard.write("\b\bχ̌")
 			elif lastKey == "C":
-				keyboard.write("\bX̌")
+				keyboard.write("\b\bX̌")
 			elif lastKey == "p":
 				keyboard.write("\bΦ")
 			elif lastKey == "P":
@@ -48,19 +53,14 @@ def handler(event: keyboard.KeyboardEvent):
 			else:
 				resetLastKey = False
 				keyboard.write("Ψ" if uppercase else "ψ")
-		case "i": keyboard.write("I" if uppercase else "ι")
-		case "j": keyboard.write("X" if uppercase else "χ")
-		case "k": 
+		case "i": 
 			if lastKey == "c":
-				resetLastKey = True
-				keyboard.send("backspace")
-				keyboard.write("κ́")
+				keyboard.write("\b\bσ́")
 			elif lastKey == "C":
-				resetLastKey = True
-				keyboard.send("backspace")
-				keyboard.write("Ḱ")
-			else:
-				keyboard.write("K" if uppercase else "κ")
+				keyboard.write("\b\bΣ́" )
+			keyboard.write("I" if uppercase else "ι")
+		case "j": keyboard.write("X" if uppercase else "χ")
+		case "k": keyboard.write("K" if uppercase else "κ")
 		case "l": keyboard.write("Λ" if uppercase else "λ")
 		case "m": keyboard.write("M" if uppercase else "μ")
 		case "n": keyboard.write("N" if uppercase else "v")
@@ -68,23 +68,18 @@ def handler(event: keyboard.KeyboardEvent):
 		case "p": keyboard.write("Π" if uppercase else "π")
 		case "q": keyboard.write("Ϟ" if uppercase else "ϰ")
 		case "r": keyboard.write("P" if uppercase else "p")
-		case "s": 
-			if lastKey == "c":
-				resetLastKey = True
-				keyboard.send("backspace")
-				keyboard.write("σ́")
-			elif lastKey == "C":
-				resetLastKey = True
-				keyboard.send("backspace")
-				keyboard.write("Σ́")
-			else:
-				keyboard.write("Σ" if uppercase else "σ")
+		case "s": keyboard.write("Σ" if uppercase else "σ")
 		case "t": keyboard.write("T" if uppercase else "τ")
 		case "u": keyboard.write("U" if uppercase else "u")
 		case "v": keyboard.write("B́" if uppercase else "β́")
 		case "w": keyboard.write("F" if uppercase else "ω")
 		case "x": keyboard.write("Ξ" if uppercase else "ξ")
-		case "y": keyboard.write("H" if uppercase else "η")
+		case "y": 
+			if lastKey == "c":
+				keyboard.write("\b\bσ́")
+			elif lastKey == "C":
+				keyboard.write("\b\bΣ́" )
+			keyboard.write("H" if uppercase else "η")
 		case "z": keyboard.write("Z" if uppercase else "ζ")
 
 	lastKey = event.name if not resetLastKey else ""
@@ -95,18 +90,17 @@ def lastKeyHandler(_=""):
 	lastKey = ""
 	pass
 
-def spacebarHandler(_):
+def SLastFormHandler(event: keyboard.KeyboardEvent):
 	global lastKey
-	if lastKey == "s" and not (keyboard.is_pressed("ctrl") or keyboard.is_pressed("alt") or keyboard.is_pressed("left windows")):
-		keyboard.send("backspace")
-		keyboard.write("ς ")
-	else:
-		keyboard.send(57)
+	if not lastKey == "s" or (keyboard.is_pressed("ctrl") or keyboard.is_pressed("alt") or keyboard.is_pressed("left windows")): return
+
+	keyboard.write("\b\bς")
+	keyboard.send(event.name)
 	lastKey = ""
 	
 mouse.on_click(lastKeyHandler)
 keyboard.on_press_key("backspace", lastKeyHandler)
-keyboard.on_press_key("space", spacebarHandler, suppress=True)
+keyboard.on_press(SLastFormHandler)
 for letter in "abcdefghijklmnopqrstuvwxyz": 
 	keyboard.on_press_key(letter, handler, suppress=True)
 
